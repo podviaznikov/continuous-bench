225cc84ebabc119df1517050d442d249d4d58a30
Tj Holowaychuk <tj@vision-media.ca>
2011-08-20 04:39:55 -0700

    setting up lib

--- app.js
+++ app.js
@@ -5,38 +5,10 @@
 
 var express = require('express');
 
-var app = module.exports = express.createServer();
+app = module.exports = express.createServer();
 
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'keyboard chaton' }));
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
-});
+require('./lib/config');
+require('./lib/routes');
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
