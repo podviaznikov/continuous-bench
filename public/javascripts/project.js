8b807bf6b17c7f666d5313c107a497941a713367
Tj Holowaychuk <tj@vision-media.ca>
2011-08-20 13:46:45 -0700

    clear commit text after request

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -12,6 +12,7 @@ o(refresh);
 o(function(){
   var commit = View(o('#commit'));
   commit.submit.json(function(res){
+    commit.commit.clear();
     if (202 == res.status) {
       dialog
         .title('Job queued')
